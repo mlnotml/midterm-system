e58b5afa3a223a7f7b870e86b8617db81d74ac47
mlnotml <mlnotmlatienza123@gmail.com>
2024-11-20 13:13:18 +0800

    Add files via upload

---
+++ online_order.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 20, 2024 at 06:12 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `online_ordering_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `online_order`
+--
+
+CREATE TABLE `online_order` (
+  `id` int(11) NOT NULL,
+  `item_no` int(11) NOT NULL,
+  `order_no` int(11) NOT NULL,
+  `transaction_id` int(11) NOT NULL,
+  `payment` varchar(255) NOT NULL,
+  `delivery` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `online_order`
+--
+
+INSERT INTO `online_order` (`id`, `item_no`, `order_no`, `transaction_id`, `payment`, `delivery`) VALUES
+(6, 12312, 2342, 425234, 'Gcash', 'Bike');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `online_order`
+--
+ALTER TABLE `online_order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `online_order`
+--
+ALTER TABLE `online_order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
